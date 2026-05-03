fd0d2f9404fd614feb68c57eb76b6a60c7bb1550
Stas Oleksiichuk <stas.oleksiichuk88@gmail.com>
2026-05-03 17:31:58 +0300

    feat(config): add configuration files

---
+++ mywebapp/config.js
@@ -0,0 +1,19 @@
+import fs from 'fs';
+
+const GLOBAL_CONFIG = '/etc/mywebapp/config.json';
+const LOCAL_CONFIG = './config.json';
+
+let configData = {};
+
+if (fs.existsSync(GLOBAL_CONFIG)) {
+  configData = JSON.parse(fs.readFileSync(GLOBAL_CONFIG));
+} else if (fs.existsSync(LOCAL_CONFIG)) {
+  configData = JSON.parse(fs.readFileSync(LOCAL_CONFIG));
+} else {
+  configData = {
+    port: 5000,
+    db_url: 'postgresql://task_user:PassTask1234@127.0.0.1:5432/task_tracker',
+  };
+}
+
+export default configData;
