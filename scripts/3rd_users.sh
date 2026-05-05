4e455bf100b5e6b3787f781bdc51d9b26c9e0c73
Stas Oleksiichuk <stas.oleksiichuk88@gmail.com>
2026-05-05 21:58:09 +0300

    fix: change teacher password policy and correct operator sudo permissions

--- scripts/3rd_users.sh
+++ scripts/3rd_users.sh
@@ -2,13 +2,28 @@
 set -e
 echo "3. Configuring users..."
 
-id -u app &>/dev/null || useradd -r -s /usr/sbin/nologin app
+getent passwd app >/dev/null || useradd -r -s /usr/sbin/nologin app
 
-id -u student &>/dev/null || { useradd -m -s /bin/bash student; echo "student:12345678" | chpasswd; usermod -aG sudo student; }
-id -u teacher &>/dev/null || { useradd -m -s /bin/bash teacher; echo "teacher:12345678" | chpasswd; usermod -aG sudo teacher; chage -d 0 teacher; }
+if ! id "student" &>/dev/null; then
+    useradd -m -s /bin/bash student
+    echo "student:12345678" | chpasswd
+    usermod -aG sudo student
+fi
 
-id -u operator &>/dev/null || { useradd -m -s /bin/bash operator; echo "operator:12345678" | chpasswd; chage -d 0 operator; }
+if ! id "teacher" &>/dev/null; then
+    useradd -m -s /bin/bash teacher
+    echo "teacher:12345678" | chpasswd
+    usermod -aG sudo teacher
+    chage -d 0 teacher
+fi
+
+getent group operator >/dev/null || groupadd operator
+if ! id "operator" &>/dev/null; then
+    useradd -m -g operator -s /bin/bash operator
+    echo "operator:12345678" | chpasswd
+    chage -d 0 operator
+fi
 
 SUDOERS_FILE="/etc/sudoers.d/operator"
-echo "operator ALL=(ALL) NOPASSWD: /usr/bin/systemctl start mywebapp.service, /usr/bin/systemctl stop mywebapp.service, /usr/bin/systemctl restart mywebapp.service, /usr/bin/systemctl status mywebapp.service, /usr/bin/systemctl reload nginx" > $SUDOERS_FILE
+echo "operator ALL=(ALL) NOPASSWD: /usr/bin/systemctl start mywebapp.service, /usr/bin/systemctl stop mywebapp.service, /usr/bin/systemctl restart mywebapp.service, /usr/bin/systemctl status mywebapp.service, /usr/bin/systemctl start mywebapp.socket, /usr/bin/systemctl stop mywebapp.socket, /usr/bin/systemctl restart mywebapp.socket, /usr/bin/systemctl status mywebapp.socket, /usr/bin/systemctl reload nginx" > $SUDOERS_FILE
 chmod 0440 $SUDOERS_FILE
